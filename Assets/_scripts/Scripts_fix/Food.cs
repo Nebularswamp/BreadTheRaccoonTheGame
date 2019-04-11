ba6712309029daab46ac3565f77e233bcd703b6f
Nebularswamp <Nebularswamp@users.noreply.github.com>
2019-04-10 21:52:07 -0400

    Changes

---
+++ Assets/_scripts/Scripts_fix/Food.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour
+{
+    public string foodName;
+
+    public int healthGain;
+    public int hungerGain;
+    public int SanityGain;
+
+    public bool inRange;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            inRange = false;
+            Debug.Log("Not in range");
+        }
+    }
+
+    void Update()
+    {
+        Eat();
+    }
+
+    void Eat()
+    {
+        if (inRange && Input.GetKey(KeyCode.E))
+        {
+            PlayerStats.instance.health += healthGain;
+            PlayerStats.instance.UpdateStats();
+            PlayerController.instance.isEating = true;
+        }
+    }
+}
---
+++ Assets/_scripts/Scripts_fix/PlayerStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats instance;
+
+    public int health = 100;
+    public int hunger = 100;
+    public int sanity = 100;
+
+    public Text displayHealth;
+
+
+    
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void UpdateStats()
+    {
+        displayHealth.text = "" + health;
+    }
+
+}
